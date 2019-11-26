6a4bbba9963c57a4e78d64394b8da3afa9156daf
nmolinare <58196590+nmolinare@users.noreply.github.com>
2019-11-25 20:14:38 -0500

    Add files via upload

---
+++ NMolinareCW3.py
@@ -0,0 +1,9 @@
+import random
+
+a = 2
+b = 20
+
+value = random.randint(a, b)
+print(value)
+
+
---
+++ NMolinareCW4.py
@@ -0,0 +1,12 @@
+a = input("Give me a number")
+print(a)
+
+b = 4
+
+import random
+a = 0
+b = 5
+c = random.randint(a,b)
+print(c)
+
+
